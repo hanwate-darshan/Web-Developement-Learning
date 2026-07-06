05793ec54838251aae631b6cdf4af10d0112dc23
hanwate-darshan <darshanmrh12@gmail.com>
2026-07-06 14:54:55 +0530

    redis

--- Advanced_Backend_development/2_Redis/phase_1/index.js
+++ Advanced_Backend_development/2_Redis/phase_1/index.js
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
 app.post("/create",async (req,res)=>{
 
     const {name ,email , password} = req.body;
+    await redis.del("user:all")
     const user = await User.create({
           name,
           email,
